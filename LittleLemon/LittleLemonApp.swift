3044adb883f5d43f248e8457e10ba44907a0c3b7
Jash Dhinoja <jash.dhinoja@gmail.com>
2023-09-13 10:14:54 -0400

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Jash Dhinoja on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
